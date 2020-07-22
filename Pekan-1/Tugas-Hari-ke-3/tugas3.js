e64ece7f085d55252cefb609d90c5ea6626bc99b
haryabdulrachman <haryabdulrachman@gmail.com>
2020-07-22 13:02:54 +0700

    Menambahkan file index.js

---
+++ Pekan-1/Tugas-Hari-ke-3/tugas3.js
@@ -0,0 +1,12 @@
+// SOAL 1
+// Gabungkan variabel tersebut menjadi "saya Senang belajar JAVASCRIPT"
+// var kataPertama = "saya";
+// var kataKedua = "senang";
+// var kataKetiga = "belajar";
+// var kataKeempat = "javascript";
+
+// var output = kataPertama + kataKedua + kataKetiga + kataKeempat;
+
+
+var sayHello = "Hello World!"
+console.log(sayHello);
